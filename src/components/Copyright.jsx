0f612a45f33d3d014d228c1b8093c4cb06a7b17a
Maddy-10 <jrmadhan10@gmail.com>
2024-03-30 21:43:35 +0530

    Added dashboard

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -4,7 +4,7 @@ const Copyright = () => {
   const currentYear = new Date().getFullYear();
   return (
     <div className='bg-black text-white text-center border-t py-2 px-7'>
-        <div className='text-[var(--primary-blue)]'>
+        <div className=''>
             <p>Copyright  &copy;<span id="currentYear"> {currentYear}</span> Designed by Wibetec. All Right Reserved</p>
         </div>
     </div>
